07a4bbdf352e200c8fcd2dbd4828030c896dd960
Kenny Murrell <kenneth.cutkomp@gmail.com>
2022-03-15 17:24:14 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**************************************************************
-* Name        : Midterm project working model
+* Name        : Midterm project final version
 * Author      : Kenny Murrell, Ian Folger, Scott Grigsby, Brennan Hydorn, Alexis Meis
-* Created     : 02/07/2022
+* Created     : 02/07/2022 - revised 3/1/2022
 * Course      : CIS 164- advanced C++
 * EMAIL       : krcutkomp@dmacc.edu
 **************************************************/
@@ -13,6 +13,7 @@
 #include<array>
 #include<fstream>
 #include<vector>
+#include<cctype>
 using namespace std;
 
 //class object to store table information
@@ -20,10 +21,14 @@ class table {
     private:
     int tableNumber;
     int seatsAvailable;
+    bool isAvailable;
+    string partyName;
     public:
-    void setTable (int number, int seats) {
+    table(int number, int seats) {
         tableNumber = number;
         seatsAvailable = seats;
+        isAvailable = true;
+        partyName = "None";
     }
     int getTableNumber() {
         return tableNumber;
@@ -31,17 +36,29 @@ class table {
     int getTableSeats() {
         return seatsAvailable;
     }
+    bool getIsAvailable() {
+        return isAvailable;
+    }
+    string getPartyName() {
+        return partyName;
+    }
+    void setPartyName(string name) {
+        partyName = name;
+    }
+    void setAvailable(bool x) {
+        isAvailable = x;
+    }
     };
 
 //function to print the vector of available tables
 void printTable(vector<vector<table>>tables) {
     cout << "Table number and (seats)" << endl;
-    for (int i = 0; i < tables.size(); i++) {
+    for (unsigned int i = 0; i < tables.size(); i++) {
         cout << i + 1 << " PM: ";
-        for(int j = 0; j < tables[i].size(); j++) {
+        for(unsigned int j = 0; j < tables[i].size(); j++) {
             table currentTable = tables[i][j];
-            if(currentTable.getTableNumber() == 0) {
-                cout << "(" << "X" << ")" << "  ";
+            if(!currentTable.getIsAvailable()) {
+                cout << "(" << currentTable.getPartyName() << ")" << "  ";
             }
             else{
                 cout << currentTable.getTableNumber() << "(" << currentTable.getTableSeats() << ")" << " ";
@@ -54,8 +71,9 @@ void printTable(vector<vector<table>>tables) {
 //function to print the top menu
 void printMenu () {
     cout << "press 1 to show tables" << endl;
-    cout << "press 2 to seat walk in" << endl;
-    cout << "press 3 to reserve call in" << endl;
+    cout << "press 2 to reserve" << endl;
+    cout << "press 3 to seat" << endl;
+    cout << "press 4 to cancel a reservation" << endl;
     cout << "press 0 to quit for the day (resets all reservations to none)" << endl;
     }
 
@@ -75,6 +93,14 @@ int tableChoiceValidator() {
     return choice;
 }
 
+//function to get input for name
+string getNameInput() {
+    string name;
+    cout << "What name is the reservation under?"<< endl;
+    cin >> name;
+    return name;
+}
+
 //function to make sure time is in range
 int timeChoiceValidator() {
     int choice;
@@ -92,20 +118,23 @@ int timeChoiceValidator() {
     return choice;
 }
 
-//function to make sure table selected is empty
-bool isTableEmpty(table currentTable) {
-    if(currentTable.getTableNumber() == 0) {
-        return false;
-    }else{
-        return true;
-    }
-    }
-
+//function to find if a name is in the vector
+bool hasTable(vector<vector<table>>tables, string x) {
+    for (unsigned int i = 0; i < tables.size(); i++) {
+        for(unsigned int j = 0; j < tables[i].size(); j++) {
+            table currentTable = tables[i][j];
+            string currParty = currentTable.getPartyName();
+            if(currParty.compare(x) == 0) {
+                return true;
+            }
+        }
+    }return 0;
+}
 
 int main()
 {
     //variables for later use in the menu
-      int menuChoice = 9; //sets the choice so the while loop menu below runs
+      int menuChoice; //sets the choice so the while loop menu below runs
       int tableChoice;
       int timeChoice;
 
@@ -113,35 +142,20 @@ int main()
       //5 tables with 4 chairs
       //5 tables with 2 chairs
       //3 tables with 10 chairs
-      table table1;
-      table1.setTable(1,4);
-      table table2;
-      table2.setTable(2,4);
-      table table3;
-      table3.setTable(3,4);
-      table table4;
-      table4.setTable(4,4);
-      table table5;
-      table5.setTable(5,4);
-      table table6;
-      table6.setTable(6,2);
-      table table7;
-      table7.setTable(7,2);
-      table table8;
-      table8.setTable(8,2);
-      table table9;
-      table9.setTable(9,2);
-      table table10;
-      table10.setTable(10,2);
-      table table11;
-      table11.setTable(11,10);
-      table table12;
-      table12.setTable(12,10);
-      table table13;
-      table13.setTable(13,10);
-      //table 0 is a placeholder for a table that is unavilable
-      table table0;
-      table0.setTable(0,0);
+      table table1(1,4);
+      table table2(2,4);
+      table table3(3,4);
+      table table4(4,4);
+      table table5(5,4);
+      table table6(6,2);
+      table table7(7,2);
+      table table8(8,2);
+      table table9(9,2);
+      table table10(10,2);
+      table table11(11,10);
+      table table12(12,10);
+      table table13(13,10);
+
 
       vector<vector<table>>tables //creates 2d vector and fills it with the tables
         {
@@ -157,52 +171,100 @@ int main()
         {table1,table2,table3,table4,table5,table6,table7,table8,table9,table10,table11,table12,table13},
         };
 
-        //final version could have second vector that stores tables reserved and names of those who reserved them
-        //or additional parameter something like 'avaliable' that can flag the table as taken
-        while(menuChoice != 0 ) {
+        //running program main loop
+        do {
             printMenu();
             cin >> menuChoice;
 
-            if(menuChoice == 1) {//prints menu
+            //print main menu
+            if(menuChoice == 1) {
                 printTable(tables);
-            }else if(menuChoice == 2) {//for walk ins
+            //to make a reservation
+            }else if(menuChoice == 2) {
                 printTable(tables);
                 timeChoice = timeChoiceValidator();
                 tableChoice = tableChoiceValidator();
-                if(isTableEmpty(tables[timeChoice-1][tableChoice-1])) { //checks that table is empty
-                    tables[timeChoice-1][tableChoice-1] = table0;
-                     //this will remove the table from the vector and replace it with table0
+                table currTable = tables[timeChoice-1][tableChoice-1];
+                //checks that table is empty
+                if(currTable.getIsAvailable()) {
+                    //changes the bool to false aka unavailable
+                    tables[timeChoice-1][tableChoice-1].setPartyName(getNameInput());
+                    tables[timeChoice-1][tableChoice-1].setAvailable(false);
                     cout << "Your reservation is at " << timeChoice << "PM and table " << tableChoice << endl;
                 }else{//if an unavailable table is chosen then we get an error and sent back to top menu
                     cout << "Sorry that time and table are taken. Please check availiable tables" << endl;
                 }
 
+                //to seat
             }else if(menuChoice == 3) {//for reservations
                 printTable(tables);
+                char choice;
+                string nameToSearch;
+                cout << "Is there a reservation? (y/n)" << endl;
+                cin >> choice;
+                //this section checks for the reservation and returns the table number if found
+                if(choice == 'y'){
+                    cout << "What name is the reservation under?" << endl;
+                    cin >> nameToSearch;
+                    for (unsigned int i = 0; i < tables.size(); i++) {
+                        for(unsigned int j = 0; j < tables[i].size(); j++) {
+                            table currentTable = tables[i][j];
+                            string currParty = currentTable.getPartyName();
+                            //cout << currParty;//this line is for testing
+                            if(currParty == nameToSearch) {
+                               int currTableNum = currentTable.getTableNumber();
+                               int currTime = i+ 1;
+                               cout << "Please seat the " << nameToSearch << " party at table " << currTableNum << " for " << currTime << "PM"<< endl;
+                            }
+
+                    }}
+
+                //if choice is anything but y then it goes into the reservation method again
+                }else{
                 timeChoice = timeChoiceValidator();
                 tableChoice = tableChoiceValidator();
-                if(isTableEmpty(tables[timeChoice-1][tableChoice-1])) {
-                    tables[timeChoice-1][tableChoice-1] = table0;
+                table currTable = tables[timeChoice-1][tableChoice-1];
+                if(currTable.getIsAvailable()) {
+                    tables[timeChoice-1][tableChoice-1].setAvailable(false);
                     cout << "Your reservation is at " << timeChoice << "PM and table " << tableChoice;
                 }else{
                     cout << "Sorry that time and table are taken. Please check availiable tables" << endl;
                 }
-                //menu choices 2 and 3 do the same thing for now but seperating it gives us more flexibility for changes
-                //and additions later
-            }else if(menuChoice == 0) {//to close and end the program
+                }
+
+                //to clear a reservation
+            }else if(menuChoice == 4){
+                cout << "What table and time do you want to clear?" << endl;
+                timeChoice = timeChoiceValidator();
+                tableChoice = tableChoiceValidator();
+                table currTable = tables[timeChoice-1][tableChoice-1];
+                if(currTable.getIsAvailable()){
+                    cout << "The table is already clear"<< endl;
+                }else{
+                    cout << "Table " << tableChoice << " for " << currTable.getPartyName() << " at " << timeChoice << " PM is now clear and ready for new reservation" << endl;
+                    currTable.setAvailable(true);
+                    currTable.setPartyName("NONE");
+                }
+
+                //to close and end the program
+            }else if(menuChoice == 0) {
                 char choice;
                 cout << "are you sure y/n (this will reset all reservations to zero)" << endl;  //this verifies they actually want to close
                 cin >> choice;
                 if(choice == 'y') {
                     cout << "Thank you have a nice day! *** PROGRAM TERMINATED ***" << endl;
-                    break;
-                } //no need for else since anything but y will just send the loop back to the top menu
+                }else {
+                    menuChoice = 9;
+                    cout << "Program exit cancelled" << endl;
+                }
+
+                //catch for any invalid input on menu choice
             }else{
                 cout << "please enter valid choice" << endl;
                 cin.clear();
                 cin.ignore(1000, '\n');
             }
-        }
+        }while(menuChoice != 0 );
 
 
 return 0;
@@ -212,7 +274,19 @@ return 0;
 //Testing
 /*
  * Entered 0 at top menu then y and program terminated with exit message
- * Entered 2 at top menu then 3 and 4 for time/table and received reservation confirmation. Table successfuly replaced with table 0 in vector
- * Tried to reserve table0 and received error message, sent back to top menu.
+ * Entered 2 at top menu then 3 and 4 for time/table, greg for name,  and received reservation confirmation.
+ * Enter 3 at top menu, then y, greg, and recieved message to seat greg at correct table and time
+ * Entered 1 at top menu after adding a few names to reservations and saw the names displayed on the tables correctly
+ * Entered 9 at top menu and received error message without crashing
+ * Entered 4 at top menu, entered reserved table information, and received successful message. Printed tables and reservation cleared as expected.
+ * ***ADDED and changed FOR FINAL SUBMISSION:
+ * -changed main program from while loop to a do while loop.
+-added a bool variable to the table class to make the available tables check more efficient.
+-added string variable to table class to hold name of group who reserved
+-changed named function setTable to a default constructor
+-added ability to seat by reservation that finds the table number and time
+-ability to clear a reservation by time and table.
+-added exit cancelled condition with output message
+-update print function to show party name of reserved tables instead of just x
 */
 
